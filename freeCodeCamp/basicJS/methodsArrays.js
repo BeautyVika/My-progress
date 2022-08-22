e63d72b3ebdde5e6d8de616848ab454ae47fd568
Beauty Vika <vikachk54@gmail.com>
2022-08-22 15:25:15 +0300

    #freecodecamp added methods arrays

---
+++ freeCodeCamp/basicJS/methodsArrays.js
@@ -0,0 +1,10 @@
+const myArrayFirst = [["John", 23], ["cat", 2]];
+const removedFromMyArrayFirst = myArrayFirst.pop()
+
+const myArraySecond = [["John", 23], ["dog", 3]];
+const removedFromMyArraySecond = myArraySecond.shift()
+
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35])
+
