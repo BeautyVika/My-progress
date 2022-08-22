6e484e5bfc6be4461a9ac70f203498383cfb1508
Beauty Vika <vikachk54@gmail.com>
2022-08-22 15:06:38 +0300

    #freecodecamp added Access Multi-Dimensional Arrays With Indexes

--- freeCodeCamp/basicJS/modifyArrayDataWithIndexes.js
+++ freeCodeCamp/basicJS/modifyArrayDataWithIndexes.js
@@ -1,3 +1,13 @@
 const myArray = [18, 64, 99];
 
 myArray[0] = 45
+
+
+const myArraySecond = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArraySecond[2][1];
