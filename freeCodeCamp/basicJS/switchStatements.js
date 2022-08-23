8e7d2c7dfa9517ffde6d1440a057de1ed74a649b
Beauty Vika <vikachk54@gmail.com>
2022-08-23 13:28:38 +0300

    #freecodecamp added switch statements

---
+++ freeCodeCamp/basicJS/switchStatements.js
@@ -0,0 +1,19 @@
+function caseInSwitch(val) {
+    let answer = "";
+    switch(val) {
+      case 1:
+         return answer ="alpha";
+       break;
+      case 2:
+         return answer ="beta";
+       break;
+      case 3:
+       return answer = "gamma";
+       break;
+      case 4:
+       return answer ="delta";
+       break;
+      default: answer
+    }
+  }
+  caseInSwitch(1);
