61b72cf6bca8f8f031fbce69c739a4ef7a3cc02c
BeautyVika <vikachk54@gmail.com>
2022-11-13 15:27:12 +0300

    added 2 tasks

---
+++ freeCodeCamp/basicJS/GenerateRandomWholeNumbers.js
@@ -0,0 +1,13 @@
+function randomWholeNum() {
+
+    return Math.floor(Math.random() * 10);
+}
+console.log(randomWholeNum())
+
+
+function randomRange(myMin, myMax) {
+
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+}
+console.log(randomRange(7, 8))
+
